26e729b97adf0364bc40501b8e58975235e067ab
Ayan <ayanchhipa2278@gmail.com>
2024-11-12 16:26:34 +0530

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import './App.css'
 import Razorpay from './razorpay';
+import Navbar from './Components/Navbar';
 
 function App() {
   
 
   return (
     <div>
+      <Navbar/>
     </div>
   )
 }

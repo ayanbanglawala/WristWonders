0477df13f985ae5677f6f69c04e380b86ff2ef1e
Ayan <ayanchhipa2278@gmail.com>
2024-11-19 16:01:32 +0530

    Routing only

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import './App.css'
-import Razorpay from './razorpay';
 import Home from './pages/Home/Home';
 import Login from './pages/Authentication/Login';
 import Signup from './pages/Authentication/Signup';
@@ -12,12 +11,24 @@ import ViewCart from './pages/Cart/ViewCart';
 import AddressSelect from './pages/Cart/AddressSelect';
 import Payment from './pages/Checkout/Payment';
 
+import { Routes, Route, Navigate } from "react-router-dom"
+
 function App() {
   
 
   return (
     <div>
-      <Payment/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/allproducts" element={<ProductsList />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/cart" element={<ViewCart />} />
+        <Route path="/address" element={<AddressSelect />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
     </div>
   )
 }

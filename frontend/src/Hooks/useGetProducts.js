2e2fa977b571bec875e2e193f431132fb3f5a8ba
Ayan <ayanchhipa2278@gmail.com>
2024-11-20 16:40:50 +0530

    a

--- frontend/src/Hooks/useGetProducts.js
+++ frontend/src/Hooks/useGetProducts.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const useGetProducts = () => {
-   
+   const [loading, setLoading] = useState(false);
 }
 
 export default useGetProducts

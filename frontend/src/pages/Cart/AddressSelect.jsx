528ae771fbcfd054ca7490600b758eda1febbd13
Ayan <ayanchhipa2278@gmail.com>
2024-11-25 17:18:37 +0530

    SOLVE

--- frontend/src/pages/Cart/AddressSelect.jsx
+++ frontend/src/pages/Cart/AddressSelect.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../../Components/Navbar';
 import { Link, useNavigate } from 'react-router-dom';
-import useAddAddress from '../../hooks/useAddAddress'; // Adjust the path as needed
+import useAddAddress from '../../Hooks/useAddAddress'; // Adjust the path as needed
 import useMakePrimary from '../../Hooks/useMakePrimary';
 
 const AddressSelect = () => {

0c5c6692bf99325144b70a123373f4d38980f17c
Ayan <ayanchhipa2278@gmail.com>
2024-12-03 16:30:59 +0530

    rating Done

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MdOutlineWatch } from "react-icons/md";
 import { Link } from "react-router-dom";
 import AddToCart from "./Cart/AddToCart";
+import { FaStar } from "react-icons/fa";
 
 const Card = ({ image, name, price, productId, rating = 0 }) => {
   return (
@@ -13,86 +14,7 @@ const Card = ({ image, name, price, productId, rating = 0 }) => {
       <div className="card-body p-3 flex justify-center items-center">
         <h2 className="card-title text-md">{name}</h2>
         <h2 className="card-title text-sm">₹{price}/-</h2>
-        <div className="rating rating-lg rating-half w-[40%] flex justify-center items-center">
-          <input
-            type="radio"
-            name="rating-10"
-            className="rating-hidden"
-            checked={rating === 0}
-            readOnly // Make the input read-only
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="mask mask-star-2 mask-half-1 bg-green-500"
-            checked={rating <= 0.75 && rating > 0.25}
-            readOnly // Add readOnly
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="mask mask-star-2 mask-half-2 bg-green-500"
-            checked={rating <= 1.25 && rating > 0.75}
-            readOnly
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="mask mask-star-2 mask-half-1 bg-green-500"
-            checked={rating <= 1.75 && rating > 1.25}
-            readOnly
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="mask mask-star-2 mask-half-2 bg-green-500"
-            checked={rating <= 2.25 && rating > 1.75}
-            readOnly
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="mask mask-star-2 mask-half-1 bg-green-500"
-            checked={rating <= 2.75 && rating > 2.25}
-            readOnly
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="mask mask-star-2 mask-half-2 bg-green-500"
-            checked={rating <= 3.25 && rating > 2.75}
-            readOnly
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="mask mask-star-2 mask-half-1 bg-green-500"
-            checked={rating <= 3.75 && rating > 3.25}
-            readOnly
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="mask mask-star-2 mask-half-2 bg-green-500"
-            checked={rating <= 4.25 && rating > 3.75}
-            readOnly
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="mask mask-star-2 mask-half-1 bg-green-500"
-            checked={rating <= 4.75 && rating > 4.25}
-            readOnly
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="mask mask-star-2 mask-half-2 bg-green-500"
-            checked={rating <= 5 && rating > 4.75}
-            readOnly
-          />
-          <p className="ml-2">{rating}</p>
-        </div>
+        <div className="badge text-white bg-blue-500 h-8 gap-2 mt-5"><FaStar/> {rating}</div>
 
         <div className="flex justify-between gap-4">
           <Link to={`/product/${productId}`}>
--- frontend/src/pages/Products Listing/Product.jsx
+++ frontend/src/pages/Products Listing/Product.jsx
@@ -6,24 +6,26 @@ import useCheckIsOrder from '../../Hooks/useCheckIsOrder';
 import AddToCart from '../../Components/Cart/AddToCart';
 import useRateProduct from '../../Hooks/useRateProduct';
 import useGetRating from '../../Hooks/useGetRating';
+import { FaStar } from "react-icons/fa";
 
 const Product = () => {
   const { id } = useParams();
   const { loading: loadingProduct, product, getProductDetails } = useGetProductDetails();
   const { loading: loadingOrder, isOrder, isOrdered, error } = useCheckIsOrder();
-  const {rateProduct1, loading} = useRateProduct();
-  const {loading1, ratings, getRatings} = useGetRating();
+  const { rateProduct1, loading } = useRateProduct();
+  const { loading1, ratings, getRatings } = useGetRating();
+  const [isRated, setRated] = useState(false);
 
   const [rateProduct, setRateProduct] = useState(1);
   const [comment, setComment] = useState("");
 
   const idOfProduct = id;
   console.log(idOfProduct);
-  
 
-  const handleRateChange = async(e) => {
+
+  const handleRateChange = async (e) => {
     e.preventDefault();
-    await rateProduct1({rating: rateProduct, comment : comment, id : idOfProduct});
+    await rateProduct1({ rating: rateProduct, comment: comment, id: idOfProduct });
     console.log("ID:", idOfProduct);
     console.log("Rating:", rateProduct);
     console.log("Comment:", comment);
@@ -34,12 +36,28 @@ const Product = () => {
     if (id) {
       getProductDetails(id);
       isOrdered(id); // Check if the product is ordered
-      if (isOrder) {
-        getRatings({id : idOfProduct});
-        console.log(ratings, "000000000000000000");
-      }
     }
   }, [id]);
+
+  useEffect(() => {
+    if (isOrder) {
+      getRatings({ id: idOfProduct });
+    }
+    else {
+      setRated(false);
+    }
+  }, [isOrder, idOfProduct, getRatings]);
+
+  useEffect(() => {
+    if (ratings.comment) {
+      setRated(true);
+    }
+    else {
+      setRated(false);
+    }
+    console.log(ratings.comment);
+  }, [ratings]);
+
 
   // Show loading spinner or fallback UI while fetching data
   if (loadingProduct) {
@@ -68,6 +86,7 @@ const Product = () => {
           <div className="text-left w-full lg:w-1/2 p-4">
             <h1 className="text-4xl font-bold">{product?.name}</h1>
             <h2 className="text-2xl font-bold">{product?.brand}</h2>
+            <div className="badge text-white bg-blue-500 h-8 gap-2 mt-5"><FaStar/> {product?.ratings}</div>
             <p className="py-6">{product?.description}</p>
 
             {/* Add to Cart Button */}
@@ -78,7 +97,7 @@ const Product = () => {
               <p>Checking order status...</p>
             ) : error ? (
               <p className="text-red-500">{error}</p>
-            ) : isOrder ? (
+            ) : isOrder && !isRated ? (
               <div className="w-100 flex justify-center py-10 flex-col gap-4">
                 <p>Rate the product!</p>
                 <div className="rating flex w-100">
@@ -102,8 +121,21 @@ const Product = () => {
                   Submit
                 </button>
               </div>
-            ) : (
-              <p className="text-gray-500">You haven't ordered this product yet.</p>
+            ) : isRated ? (<div className=' flex flex-col card w-100 justify-center items-center gap-3 my-5'>
+              <div className="card bg-base-100 w-96 shadow-xl">
+                <div className="card-body flex justify-center text-center items-center">
+                  <div className="rating flex w-100">
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-blue-400" defaultChecked={ratings.rating == 1} disabled />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-blue-400" defaultChecked={ratings.rating == 2} disabled />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-blue-400" defaultChecked={ratings.rating == 3} disabled />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-blue-400" defaultChecked={ratings.rating == 4} disabled />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-blue-400" defaultChecked={ratings.rating == 5} disabled />
+                  </div>
+                  <p>Your rating : {ratings.rating}</p>
+                  <p>Your Comment : {ratings.comment}</p>
+                </div></div>
+            </div>) : (
+              <p className="text-gray-500"></p>
             )}
           </div>
         </div>
